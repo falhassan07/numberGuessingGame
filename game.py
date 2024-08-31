d5424074b66290d7e8886e1e977f8883778ffcaf
Farouk Alhassan <omarfarouk@farouks-mbp.home>
2024-08-31 14:09:35 -0400

    added guess_guess function skeleton

---
+++ game.py
@@ -0,0 +1,14 @@
+import random 
+from art import logo 
+
+
+print(logo)
+print("Welcome to the Number Guessing Game!")
+print("Im thinking of a number between 1 and 100")
+
+
+def guess_game():
+    pass
+
+
+guess_game()
