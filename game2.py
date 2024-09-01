5bc357777ee8be9900b545dbad2be325a487c277
Farouk Alhassan <omarfarouk@farouks-mbp.home>
2024-09-01 13:45:54 -0400

    added check_answer function

--- game2.py
+++ game2.py
@@ -1,3 +1,19 @@
 from random import randint
 from art import logo
 
+
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+
+# Function to check users' guess against actual answer
+def check_answer(user_guess, actual_answer, turns):
+    """Checks answer against guess, returns the number of turns remaining."""
+    if user_guess > actual_answer:
+        print("Too high.")
+        return turns - 1
+    elif user_guess < actual_answer:
+        print("Too low.")
+        return turns - 1
+    else:
+        print(f"You got it! The answer was {actual_answer}")
